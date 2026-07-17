3c78014d0b3fea100d8b8a72311acb6bc0eaed00
Abhaykumar39 <124204133+Abhaykumar39@users.noreply.github.com>
2026-07-17 18:45:38 +0530

    Time: 25 ms (5.03%), Space: 53 MB (5.02%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public String countAndSay(int n) {
+        if(n==1)
+        {
+            return "1";
+        }
+        String s = countAndSay(n-1);
+
+        String ans = "";
+        int i=0,j=0;
+        while(j<s.length())
+        {
+            if(s.charAt(i)==s.charAt(j))
+            {
+                j++;
+            }
+            else
+            {
+                int freq = j-i;
+                ans += freq;
+                ans+= s.charAt(i);
+                i=j;
+            }
+        }
+        //Last character count not fullfill so pick it last with digit.
+        int freq = j - i;
+        ans += freq;
+        ans+= s.charAt(i);
+        return ans;
+
+    }
+}
